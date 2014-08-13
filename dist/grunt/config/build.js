dd0ae60b11dac48593a50ec0a7a6fe7401f1d695
Valeriy Shkiryak <lizard@aboutecho.com>
2014-08-13 12:55:36 +0400

    Remove task that is not always executed in build pipeline, applications should add this task themselves if needed

--- dist/grunt/config/build.js
+++ dist/grunt/config/build.js
@@ -2,12 +2,10 @@ module.exports = {
 	options: {
 		tasks: {
 			dev: [
-				'copy:third-party',
 				'copy:js',
 				'concat'
 			],
 			min: [
-				'copy:third-party',
 				'copy:js',
 				'uglify',
 				'concat'
