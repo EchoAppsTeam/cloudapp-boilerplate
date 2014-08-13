9b7b40ad53ac25a64cc14041570646afba7c4c0a
Valeriy Shkiryak <lizard@aboutecho.com>
2014-08-13 12:49:34 +0400

    Allow "?" and ":" symbols in ternary operator to be in the line beginning

--- dist/grunt/config/jscs.js
+++ dist/grunt/config/jscs.js
@@ -1,12 +1,20 @@
-module.exports = {
-	options: {
-		preset: 'google',
-		validateIndentation: '\t',
-		maximumLineLength: {
-			value: 100
+module.exports = function() {
+	'use strict';
+	var jscs = require('jscs/lib/utils');
+	return {
+		options: {
+			preset: 'google',
+			validateIndentation: '\t',
+			// value "true" for this parameter requires ternary operator symbols (? & :)
+			// to be in the end of the line and not in the beginning,
+			// let's not force the rule for this symbol but keep for all others in place
+			requireOperatorBeforeLineBreak: jscs.binaryOperators,
+			maximumLineLength: {
+				value: 100
+			}
+		},
+		grunt: {
+			src: '<%= jshint.grunt.src %>'
 		}
-	},
-	grunt: {
-		src: '<%= jshint.grunt.src %>'
-	}
+	};
 };
